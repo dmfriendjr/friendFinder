6a4ca9d13ea5cd76fdc005a44c529b6962bd03a7
dmfriendjr <davidmfriendjr@gmail.com>
2018-02-05 18:09:29 -0500

    Changed photos so each of the starting users have distinct photos

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,13 +2,13 @@
 
 let friends = [
 		{
-			"name":"Ahmed",
-			"photo":"https://static.pexels.com/photos/10517/pexels-photo-10517.jpeg",
+			"name":"Tim",
+			"photo":"https://images.pexels.com/photos/428331/pexels-photo-428331.jpeg",
 			"scores":[5,1,4,4,5,1,2,5,4,1]
 		},
 		{
 			"name":"Rob",
-			"photo":"https://static.pexels.com/photos/10517/pexels-photo-10517.jpeg",
+			"photo":"https://images.pexels.com/photos/428328/pexels-photo-428328.jpeg",
 			"scores":[2,3,1,4,5,3,2,4,3,2]
 		}, 
 		{
